cf38f033b271b48833594b13dd8fb5efbed74fd5
PabloHazan <phazan.fiuba@gmail.com>
2014-11-04 14:47:21 -0300

    se agregó Test y comportamiento en Residencial según la cantidad de gente que entra y sale

--- test/fiuba/algo3/ResidencialTest.java
+++ test/fiuba/algo3/ResidencialTest.java
@@ -10,8 +10,40 @@ public class ResidencialTest extends TestCase {
 	
 	public void testAgregarUnHabitante(){
 		Residencial edificio = new Residencial();
-		edificio.agregarHabitantes(1);
-		assertTrue( 99 , edificio.disponibilidad());
+		try{
+			edificio.agregarHabitantes(1);
+		}catch (EspacioInsuficiente e){}
+		assertEquals (99, edificio.disponibilidad());
 	}
-
+	
+	public void testSuperarLaCantidadDeHabitantesLanzaExcepcion(){
+		Residencial edificio = new Residencial();
+		boolean excepcionLanzada = false;
+		try{
+			edificio.agregarHabitantes(101);
+		}catch (EspacioInsuficiente e){
+			excepcionLanzada = true;
+		}
+		assertTrue (excepcionLanzada);
+	}
+	
+	public void testAlAgregarDosPersonasYSacarUnaQuedan99Disponibles (){
+		Residencial edificio = new Residencial();
+		try{
+			edificio.agregarHabitantes(2);
+			edificio.quitarHabitantes(1);
+		}catch(Exception e){}
+		assertEquals (99,edificio.disponibilidad());
+	}
+	
+	public void TestQuitarHabitanteDeUnEdificioVacioLanzaUnaExcepcion(){
+		Residencial edificio = new Residencial();
+		boolean excepcionLanzada = false;
+		try{
+			edificio.quitarHabitantes(1);
+		}catch(NoHayTantosHabitantes e){
+			excepcionLanzada = true;
+		}
+		assertTrue (excepcionLanzada);
+	}
 }
