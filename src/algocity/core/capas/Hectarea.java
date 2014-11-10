31ce5bc38109a928cd046e1c4da601df65f47b60
PabloHazan <phazan.fiuba@gmail.com>
2014-11-09 21:01:32 -0300

    se puede agregar construibles en llanuras y agua segun corresponda

--- src/algocity/core/capas/Hectarea.java
+++ src/algocity/core/capas/Hectarea.java
@@ -1,7 +1,17 @@
 package algocity.core.capas;
 
 import algocity.core.CalculadorDeCalidadDeVida;
+import algocity.core.construibles.Construible;
 
 public abstract class Hectarea 
 implements CalculadorDeCalidadDeVida {
+	
+	int ConexionesElectricas;
+	Construible construible;
+	
+	public Hectarea() {
+		ConexionesElectricas = 0;
+	}
+	
+	public abstract boolean agregarConstruible(Construible construible);
 }
