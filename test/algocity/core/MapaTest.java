632395f45da717a795e501729c504f664d0a5de3
PabloHazan <phazan.fiuba@gmail.com>
2014-11-11 20:37:42 -0300

    recuperando todo

--- test/algocity/core/MapaTest.java
+++ test/algocity/core/MapaTest.java
@@ -1,9 +1,10 @@
 package algocity.core;
 
+import algocity.core.capas.HectareaLlana;
 import junit.framework.TestCase;
 
 public class MapaTest extends TestCase {
-	
+
 		public void testCrearUnMapaDe10x10PermiteCargar100Hectareas() {
 			int i = 0;
 			Mapa mapa = new Mapa(10,10);
@@ -12,4 +13,5 @@ public class MapaTest extends TestCase {
 			}
 			assertEquals (100, i);
 		}
+
 }
