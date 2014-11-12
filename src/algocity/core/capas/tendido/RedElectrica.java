0b8ca8f3a458b3032bee3c0944415283d408c6c5
Esteban A. Bellegarde <esteban.bellegarde@gmail.com>
2014-11-12 20:20:04 -0300

    Agregada la clase RedElectrica.

---
+++ src/algocity/core/capas/tendido/RedElectrica.java
@@ -0,0 +1,67 @@
+package algocity.core.capas.tendido;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RedElectrica extends Tendido {
+
+	ArrayList<Coordenada> coordenadasCentralesElectricas;
+	
+	public RedElectrica (){
+		coordenadasCentralesElectricas = new ArrayList<Coordenada>();
+	}
+	
+	@Override
+	public void agregarNodo(int coordenadaX, int coordenadaY)
+			throws NoHayNodoCercanoException, NodoExistenteException {
+		
+		if (this.getNodo(coordenadaX,coordenadaY) != null){
+			throw new NodoExistenteException();
+		}
+		
+		NodoTendido nodoNuevo = new NodoTendido(coordenadaX, coordenadaY);
+		
+		ArrayList<NodoTendido> vecinosPosibles = new ArrayList<NodoTendido>(); 
+		vecinosPosibles.add(this.getNodo(coordenadaX+1, coordenadaY));
+		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY+1));
+		vecinosPosibles.add(this.getNodo(coordenadaX-1, coordenadaY));
+		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY-1));
+		
+		/*--------------------------------------------------------------------------------------------
+		 * Agrega el nodo si son las coordenadas de una central
+		 *------------------------------------------------------------------------------------------*/
+		Iterator<Coordenada> iter2 = coordenadasCentralesElectricas.iterator();
+		while (iter2.hasNext()){
+			Coordenada coordenadaActual = iter2.next();
+			if ( (coordenadaActual.getX() == coordenadaX) && (coordenadaActual.getY() == coordenadaY) ){
+				this.nodos.add(nodoNuevo);
+				return;
+			}
+		}
+		
+		/*--------------------------------------------------------------------------------------------
+		 * Agrega el nodo si hay algun nodo vecino
+		 *------------------------------------------------------------------------------------------*/
+		
+		boolean hayVecinos = false;
+		Iterator<NodoTendido> iter = vecinosPosibles.iterator();
+		
+		while (iter.hasNext()){
+			NodoTendido nodoActual = iter.next();
+			if (nodoActual != null){
+				hayVecinos = true;
+				hacerVecinos (nodoNuevo, nodoActual);
+			}
+		}
+		
+		if (hayVecinos){
+			this.nodos.add(nodoNuevo);
+			return;
+		}
+		throw new NoHayNodoCercanoException();
+
+	}
+	
+	
+
+}
