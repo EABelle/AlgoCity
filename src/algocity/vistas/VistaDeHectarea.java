0e2486cc8a7e2b4396ba9c8e37513c8bbbf07711
Gavrilov Seva <gavrilovseva@gmail.com>
2014-11-30 18:41:39 -0300

    Ahora en la vista de la hectarea no se muestra el construible borrado.

--- src/algocity/vistas/VistaDeHectarea.java
+++ src/algocity/vistas/VistaDeHectarea.java
@@ -30,16 +30,24 @@ public class VistaDeHectarea extends JPanel implements Observer {
 		actualGraphics = g;
 
 		g.setFont(new Font("helvetica", Font.BOLD, 12));
-		if (this.construible != null) {
-			actualGraphics.setColor(Color.BLACK);
-			actualGraphics.drawString(construible, 10, 10);
-		}
+
+		printConstruible();
 		if (hectarea.redDeAguaConectada()) {
 			actualGraphics.setColor(Color.BLUE);
 			actualGraphics.drawString("a", 10, 30);
 		}
 		hectarea.dibujarse(this);
 
+	}
+
+	private void printConstruible() {
+		if (hectarea.getConstruible() == null) {
+			this.construible = null;
+		}
+		if (this.construible != null) {
+			actualGraphics.setColor(Color.BLACK);
+			actualGraphics.drawString(construible, 10, 10);
+		}
 	}
 
 	public void dibujarHectarea(HectareaLlana hectarea) {
