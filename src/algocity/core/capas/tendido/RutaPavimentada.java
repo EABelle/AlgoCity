9503b90ffe7391633e0919f713210e3bf2192db6
Esteban A. Bellegarde <esteban.bellegarde@gmail.com>
2014-12-01 17:31:42 -0300

    Refactorizada la ruta pavimentada

--- src/algocity/core/capas/tendido/RutaPavimentada.java
+++ src/algocity/core/capas/tendido/RutaPavimentada.java
@@ -12,28 +12,7 @@ public class RutaPavimentada extends Tendido {
 	@Override
 	public boolean agregarNodo(int coordenadaX, int coordenadaY){
 
-		if ( this.nodoExiste(coordenadaX,coordenadaY) )
-			return false;
-
-		NodoTendido nodoNuevo = new NodoTendido(coordenadaX, coordenadaY);
-
-		ArrayList<NodoTendido> vecinosPosibles = new ArrayList<NodoTendido>();
-		vecinosPosibles.add(this.getNodo(coordenadaX+1, coordenadaY));
-		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY+1));
-		vecinosPosibles.add(this.getNodo(coordenadaX-1, coordenadaY));
-		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY-1));
-
-		Iterator<NodoTendido> iter = vecinosPosibles.iterator();
-
-		while (iter.hasNext()){
-			NodoTendido nodoActual = iter.next();
-			if (nodoActual != null){
-				hacerVecinos (nodoNuevo, nodoActual);
-			}
-		}
-
-		this.nodos.add(nodoNuevo);
-		return true;
+		return regenerarNodo( coordenadaX,  coordenadaY);
 	}
 
 }
