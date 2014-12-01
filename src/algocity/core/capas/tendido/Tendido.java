8944daa8da12895eb6715f117c563844eaabb7ad
Esteban A. Bellegarde <esteban.bellegarde@gmail.com>
2014-12-01 17:28:23 -0300

    Agregado el metodo regenerarNodo en Tendido. Utilizado para persistencia.

--- src/algocity/core/capas/tendido/Tendido.java
+++ src/algocity/core/capas/tendido/Tendido.java
@@ -123,6 +123,32 @@ abstract public class Tendido {
 		return false;
 	}
 
+	public boolean regenerarNodo(int coordenadaX, int coordenadaY){
+
+		if ( this.nodoExiste(coordenadaX,coordenadaY) )
+			return false;
+
+		NodoTendido nodoNuevo = new NodoTendido(coordenadaX, coordenadaY);
+
+		ArrayList<NodoTendido> vecinosPosibles = new ArrayList<NodoTendido>();
+		vecinosPosibles.add(this.getNodo(coordenadaX+1, coordenadaY));
+		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY+1));
+		vecinosPosibles.add(this.getNodo(coordenadaX-1, coordenadaY));
+		vecinosPosibles.add(this.getNodo(coordenadaX, coordenadaY-1));
+
+		Iterator<NodoTendido> iter = vecinosPosibles.iterator();
+
+		while (iter.hasNext()){
+			NodoTendido nodoActual = iter.next();
+			if (nodoActual != null){
+				hacerVecinos (nodoNuevo, nodoActual);
+			}
+		}
+
+		this.nodos.add(nodoNuevo);
+		return true;
+	}
+	
 	public boolean existeConexionBFS (int XInicial, int YInicial, int XFinal, int YFinal){
 		//Implementa el algoritmo de recorrido BFS para saber si dos elementos se encuentran en el mismo subgrafo conexo.
 		
